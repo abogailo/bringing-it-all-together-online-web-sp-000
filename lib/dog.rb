a340028b53bc4dd378f5987bef3382ee905ac242
Abagail Hudson <abagailanne@gmail.com>
2019-07-07 23:42:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@ class Dog
 
    attr_accessor :name, :breed, :id
 
-   def initialize( name:, breed:, id:nil)
-      @id = id,
-      @name = name,
+   def initialize( name:, breed:,id: nil)
+      @id = id
+      @name = name
       @breed = breed
    end
 
