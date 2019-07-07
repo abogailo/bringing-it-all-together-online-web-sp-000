fee467389fdcaf4b722eb353eb768ad9fe2e4a4c
Abagail Hudson <abagailanne@gmail.com>
2019-07-07 23:02:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
   def self.new_from_db(row)
-  	new_dog = self.new 
+  	new_dog = self.new
   	new_dog.id = id:row[0]
   	new_dog.name =  name:row[1]
   	new_dog.breed = breed:row[2]
-  	new_dog    
+  	new_dog
   	pat = Dog.new(id:row[0],name:row[1],breed:row[2])
-  	pat    
+  	pat
   end
 
 end
